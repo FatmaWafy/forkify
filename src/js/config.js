894131d33e83373fb0d0507d9e4d1e96d3d7b175
FatmaWafy <fatmaa1601@gmail.com>
2025-03-25 15:51:07 +0200

    upload all files for forkiy project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f1724528-7f7b-48f6-b354-9df48c7fed4a';
+export const MODAL_CLOSE_SEC = 2.5;
